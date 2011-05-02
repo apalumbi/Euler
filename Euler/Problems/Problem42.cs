3c6c17e80df4ca785658e032f5809274881d0fef
Anthony Palumbi <apalumbi@gmail.com>
2011-05-02 12:43:22 -0400

    added a permutation generator and solved problem42

---
+++ Euler/Problems/Problem42.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Euler.Problems {
+	public class Problem42 : Problem {
+		List<int> divisors = new List<int> { 2, 3, 5, 7, 11, 13, 17 };
+
+
+		public override string Solve() {
+			var generator = new PermutationGenerator<string>(new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+			BigInteger sum = 0;
+			while (generator.hasMore()) {
+			  var perm = generator.getNext();
+
+			  sum += HasSpecialProperty(perm.Select(e => e.Second).ToList());
+			}
+
+			return sum.ToString();
+		}
+
+		BigInteger HasSpecialProperty(List<string> numberString) {
+			var index = 1;
+			bool wasDivisible = true;
+			foreach (var divisor in divisors) {
+				if (index > 7) {
+					break;
+				}
+
+				var first = numberString[index];
+				var second = numberString[index + 1];
+				var third = numberString[index + 2];
+				var number = BigInteger.Parse(first + second + third);
+
+				if ((number % divisor) != 0) {
+					wasDivisible = false;
+					break;
+				}
+				index++;
+			}
+
+			return wasDivisible ? BigInteger.Parse(string.Join("", numberString)) : 0;
+		}
+	}
+}
