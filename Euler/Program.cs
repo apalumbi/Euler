8a3bde53c46e7b29ad857a2d73e7ff61850edd95
Anthony Palumbi <apalumbi@gmail.com>
2011-05-12 14:50:56 -0400

    optimized convert to base.

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -9,7 +9,7 @@ namespace Euler {
 		[STAThread]
 		static void Main(string[] args) {
 			var problems = GetAllClasses("Euler.Problems");
-			//var problems = new List<Problem> { new RoseCode06() };
+			//var problems = new List<Problem> { new Problem38() };
 			var start = DateTime.Now;
 
 			var results = new List<Result>();
